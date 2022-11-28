5993d99beaf69b885eec9bf921d4bf081a6ac174
Sammy Mureithi <sammymureithi20@gmail.com>
2022-11-28 14:16:24 +0300

    All Index pages added successfully

---
+++ MyFirtFullStackApp/Stacks/index.js
@@ -0,0 +1,31 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from '../Screens/Login';
+import SignUp from '../Screens/SignUp';
+import ForgotPassword from '../Screens/ForgotPassword';
+
+export default function Index() {
+  const FirstStack = createNativeStackNavigator();
+  return (
+    <FirstStack.Navigator>
+      <FirstStack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <FirstStack.Screen
+        name="Sign Up"
+        component={SignUp}
+        options={{headerShown: false}}
+      />
+      <FirstStack.Screen
+        name="ForgotPassword"
+        component={ForgotPassword}
+        options={{headerShown: false}}
+      />
+    </FirstStack.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({});
